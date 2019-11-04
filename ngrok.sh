79dcae73ffdc431b85e277768525e61b24bca829
T3rmuxtruc0 <57348331+T3rmuxtruc0@users.noreply.github.com>
2019-11-04 04:39:52 -0500

    Add files via upload
    Ngrok con un solo click

---
+++ ngrok.sh
@@ -0,0 +1,29 @@
+#!bin/bash
+#creado by T3rmux Truc0
+echo "HOLA DEJAME INSTALAR NGROK POR TI"
+sleep 3
+	setterm -foreground red
+
+	figlet suscribete
+echo " https://www.youtube.com/channel/UCojZjUzwLbkL6BEYv_azdrQ"
+sleep 5
+
+#comenzando......
+
+setterm -foreground green
+
+echo " ##############################"
+ figlet -f bubble t3rmux Truc0
+#instalaciones
+setterm -foreground yellow
+echo "##############################"
+apt update && apt upgrade y-
+apt install git -y 
+git clone https://github.com/tchelospy/termux-ngrok.git; cd termux-ngrok
+chmod +x termux-ngrok.sh
+./termux-ngrok.sh
+#fin
+
+
+
+
